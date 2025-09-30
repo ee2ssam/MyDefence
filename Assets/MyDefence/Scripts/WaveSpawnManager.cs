6d59dbf4e6f5a847d00a4613e44d58ad31c5ca60
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-09-30 16:26:37 +0900

    1. 터렛 설치 및 적 타겟팅 구현

--- Assets/MyDefence/Scripts/WaveSpawnManager.cs
+++ Assets/MyDefence/Scripts/WaveSpawnManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 namespace MyDefence
 {
@@ -20,6 +21,9 @@ namespace MyDefence
 
         //웨이브 카운트
         private int waveCount = 0;
+
+        //UI - Text
+        public TextMeshProUGUI countdownText;
         #endregion
 
         #region Unity Event Method
@@ -40,6 +44,13 @@ namespace MyDefence
                 //타이머 초기화
                 countdown = 0f;
             }
+
+            //UI - 카운트다운 텍스트
+            //countdown 특정 범위(min, max) 설정 (- 값이 안되도록 설정)
+            countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+            //countdownText.text = string.Format("{0:00.00}", countdown); //실수(소수점 이하) 출력
+            countdownText.text = Mathf.Round(countdown).ToString();       //반올림하여 정수 출력
+
         }
         #endregion
 
