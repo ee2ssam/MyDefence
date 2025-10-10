be2b8e3ff7b49e03b702e2eee76bd6e0a3a530ce
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-10-10 17:04:04 +0900

    1. 카메라 제어 - 인풋 시스템 구현

--- Assets/_Sample/06ObjectTest/GameObjectTest.cs
+++ Assets/_Sample/06ObjectTest/GameObjectTest.cs
@@ -10,6 +10,8 @@ gameobject 또는 transform 인스턴스(객체)를 가져오는 방법
 [3] Find - 유니티에서 제공하는 API를 이용하여 gameobject, transform 인스턴스(객체)를 반환받아
     가져온다 (ex GameObject.FindGameObjectWithTag, GameObject.FindGameObjectsWithTag..)
 [4] Prefab 게임오브젝트 생성시 Instantiate 함수의 반환값으로 gameobject 객체를 가져온다
+[5] 같은 종류, 같은 기능들로 묶어져 있는 게임오브젝트의 gameobject, transform 인스턴스(객체) 가져오기
+    부모 오브젝트를 만들고 같은 종류, 같은 기능가진 오브젝트들을 자식 오브젝트로 추가한다
 */
 
 namespace Sample
@@ -27,14 +29,14 @@ namespace Sample
         //[4] Prefab 게임오브젝트 생성시 Instantiate 함수의 반환값으로 gameobject 객체를 가져온다
         public GameObject gameobjectPrefab;
 
-        //[5] 같은 종류, 같은 기능들로 묶어져 있는 게임오브젝트의 gameobject, transform 인스턴스(객체) 가져오기
-        //부모 오브젝트를 만들고 같은 종류, 같은 기능가진 오브젝트들을 자식 오브젝트로 추가한다
-        //보모 오브젝트의 객체를 통해 자식 오브젝트들의 gameobject, transform 인스턴스(객체)에 접근
+        
+        //[5]부모 오브젝트의 객체를 통해 자식 오브젝트들의 gameobject, transform 인스턴스(객체)에 접근
         public Transform parentObject;
         private Transform[] childObjects;
 
         //[6] static 성질 이용: 싱글톤 패턴, 
         // static 필드: 클래스이름.필드이름
+        //private Singleton singleton;
 
 
         private void Start()
